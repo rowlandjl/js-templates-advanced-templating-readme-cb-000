50fa8eb3530841ec6a35d1632f98c711dbdff411
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-12 21:17:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ Handlebars.registerHelper('comment_body', function() {
 function loadIssue() {
   var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
   
-  for(var i=0; i < issues.length; i++) {
-    var result = template(issues[i]);
-    document.getElementsByTagName("main")[0].innerHTML += result;
-  }
+  var result = template(issues);
+  document.getElementsByTagName("main")[0].innerHTML += result;
   
 }
