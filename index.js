5741c134c0b227c3d73aa4fff2e6e1091b120435
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-12 21:05:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function loadIssue() {
-  var issue = {
-    state: "closed",
-    number: 5,
-    created_at: "2016-03-31 16:23:13 UTC",
-    body: "Instructions say GET /team and POST /newteam. Rspec wants GET/newteam and POST/team."
-  }
+  // var issue = {
+  //   state: "closed",
+  //   number: 5,
+  //   created_at: "2016-03-31 16:23:13 UTC",
+  //   body: "Instructions say GET /team and POST /newteam. Rspec wants GET/newteam and POST/team."
+  // }
   
   var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
   
